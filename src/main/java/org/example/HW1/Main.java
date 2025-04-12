496b3ad2405765cc58b6f918958ce15a1e9d91a3
Almaz <almaz_7785@mail.ru>
2025-04-12 20:12:28 +0300

    Break a first homework

--- src/main/java/org/example/HW1/Main.java
+++ src/main/java/org/example/HW1/Main.java
@@ -1,10 +1,29 @@
-package org.example;
+package org.example.HW1;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//Напишите программу, которая использует Stream API для обработки списка чисел.
+// Программа должна вывести на экран среднее значение всех четных чисел в списке.
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalDouble;
+
 public class Main {
     public static void main(String[] args) {
 
+        List<Integer> list = new ArrayList<>(Arrays.asList(1,-4,3,4,5,6,7,-8,9,-10));
 
+        OptionalDouble average = list.stream().
+                filter(a -> a % 2 == 0)
+                .mapToInt(Integer::intValue)
+                .average();
+        double averageDouble = average.getAsDouble();
+
+        if (averageDouble == 0) {
+            System.out.println("Нет четных числе в массиве");
+        }
+        else {
+            System.out.println("Среднее значение всех четных чисел в массиве = " + averageDouble);
+        }
     }
 }
